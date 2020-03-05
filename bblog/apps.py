06aa0c8926edc1d1d08ef123c4b992de5677b6ed
maru-ma <maruma.morandini@gmail.com>
2020-03-05 12:49:54 -0300

    My app from Django Girls Tutorial, first commit

---
+++ bblog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BblogConfig(AppConfig):
+    name = 'bblog'
---
+++ bblog/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    # Atributos.
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date =  models.DateTimeField(default=timezone.now)
+    publish_date = models.DateTimeField(blank=True, null=True)
+
+    # Metodos.
+    def publish(self):
+        self.publish_date = timezone.now
+        self.save()
+
+    # Convertimos en string lo que queremos mostrar.
+    def __str__(self):
+        return self.title
